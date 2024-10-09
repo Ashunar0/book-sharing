649e8d978f8d865984d9ebb7bd3af55edb366373
Ashunar0 <asahi.gaia1530@gmail.com>
2024-10-09 23:49:24 +0900

    ホーム画面のui変更

--- src/Home.jsx
+++ src/Home.jsx
@@ -43,48 +43,86 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      <header className="header-band">
-        <img id="icon" src="images/icon.png" alt="icon" />
-        <h2 id="title">Book-Sharing</h2>
-
-        {/* ユーザー名の表示 */}
-        {auth.currentUser && (
-          <h2 id="user-name">
-            {usernames[auth.currentUser.uid] || auth.currentUser.email}
-          </h2>
-        )}
-      </header>
-
-      <h2 className="page-title">ホーム画面</h2>
-
-      <div className="sidebar">
-        <Link to="/profile">
-          <button>プロフィール</button>
-        </Link>
-        <Link to="/create-post">
-          <button>投稿作成</button>
-        </Link>
-        <Link to="/login">
-          <button onClick={handleLogout}>ログアウト</button>
-        </Link>
-      </div>
-
-      {/* 投稿の表示 */}
-      <div className="posts-container">
-        {posts.map((post) => (
-          <div key={post.id} className="post">
-            <div className="post-user-info">
-              <img id="user-image" src="images/user_icon.png" alt="user-icon" />
-              <h3 id="post-user-name">{usernames[post.userId] || "Unknown"}</h3>
-              <p id="post-created-at">
-                {new Date(post.createdAt.seconds * 1000).toLocaleString()}
-              </p>
-            </div>
-            <h3 className="post-title">{post.title}</h3>
-            <h4 className="post-content">{post.content}</h4>
-            {/* ユーザー名の表示 */}
+      <div className="main-container">
+        <div className="sidebar">
+          <div className="title-and-icon">
+            <img id="icon" src="images/icon.png" alt="icon" />
+            <h2 id="title">Book-Sharing</h2>
           </div>
-        ))}
+          <div className="user-profile-info">
+            <img
+              src="images/user_icon.png"
+              alt="user-icon"
+              id="user-profile-icon"
+            />
+            {/* ユーザー名の表示 */}
+            {auth.currentUser && (
+              <h2 id="user-name">
+                {usernames[auth.currentUser.uid] || auth.currentUser.email}
+              </h2>
+            )}
+          </div>
+          <div className="sidebar-buttons">
+            <Link to="/profile">
+              <button className="sidebar-button">プロフィール</button>
+            </Link>
+            <Link to="/create-post">
+              <button className="sidebar-button">投稿作成</button>
+            </Link>
+            <Link to="/login">
+              <button className="sidebar-button" onClick={handleLogout}>
+                ログアウト
+              </button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="timeline">
+          <h2 className="page-title">タイムライン</h2>
+
+          {/* 投稿の表示 */}
+          <div className="posts-container">
+            {posts.map((post) => (
+              <div key={post.id} className="post">
+                <div className="post-user-info">
+                  <img
+                    id="user-image"
+                    src="images/user_icon.png"
+                    alt="user-icon"
+                  />
+                  <h3 id="post-user-name">
+                    {usernames[post.userId] || "Unknown"}
+                  </h3>
+                  <p id="post-created-at">
+                    {new Date(post.createdAt.seconds * 1000).toLocaleString()}
+                  </p>
+                </div>
+                <h3 id="post-title">{post.title}</h3>
+                <h4 id="post-content">{post.content}</h4>
+                <div className="responces">
+                  <img
+                    src="images/icons/reply.png"
+                    alt="reply_icon"
+                    className="responce-icon"
+                    id="reply"
+                  />
+                  <p className="responce-count" id="reply-count">
+                    0
+                  </p>
+                  <img
+                    src="images/icons/favorite.png"
+                    alt="like_icon"
+                    className="responce-icon"
+                    id="like"
+                  />
+                  <p className="responce-count" id="like-count">
+                    0
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
